a156b9a6bd1edef798b8e5bfc26017b5762319ec
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2015-01-13 15:53:11 -0200

    echo

--- configs/post-setup.sh
+++ configs/post-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd ~/nit
+echo "compiling srt.nit..."
 nitc srt.nit && sudo mv srt /usr/local/bin
+rm -rf .nit_compile
+cd ~/git/configs/torrents
+echo "compiling wg.nit..."
 nitc wg.nit && sudo mv wg /usr/local/bin
 rm -rf .nit_compile
 echo "nit utilities installed ok"
